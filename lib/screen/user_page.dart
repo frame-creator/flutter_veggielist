6bc50de68a98b0b2f2bdf7906443e628feaac924
code-framer <journeyofman@naver.com>
2021-06-29 20:02:57 +0900

    유저 개별 페이지 유저 프로필 UI 추가 및 스크롤 에러 해결

--- lib/screen/user_page.dart
+++ lib/screen/user_page.dart
@@ -10,7 +10,9 @@ class UserPage extends StatefulWidget {
   UserPageState createState() => UserPageState();
 }
 
-class UserPageState extends State<UserPage> {
+class UserPageState extends State<UserPage> with TickerProviderStateMixin {
+  TabController tabController;
+
   final userdata = GetStorage();
   final UserController usercontroller = Get.put(UserController());
   @override
@@ -21,147 +23,229 @@ class UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Obx(() {
-        if (usercontroller.isLoading.value == false) {
-          return Center(
-              child: CircularProgressIndicator(
-            backgroundColor: Colors.yellow,
-          ));
-        } else {
-          return Expanded(
-              child: ListView.builder(
-                  itemCount: usercontroller.places.length,
-                  itemBuilder: (BuildContext context, index) {
-                    var singlePlace = usercontroller.places[index];
-                    return Padding(
-                        padding: EdgeInsets.all(17.0),
-                        child: InkWell(
-                            onTap: () {},
-                            child: Stack(children: <Widget>[
-                              Container(
-                                height: 270.0,
-                                width: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    image: DecorationImage(
-                                        image: NetworkImage(singlePlace.image),
-                                        //              //  '${recipepost['img_url']}'),
-                                        fit: BoxFit.cover)),
-                              ),
-                              //make the shade a bit deeper.
-                              Container(
-                                height: 270.0,
-                                width: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    color: Colors.black.withOpacity(0.3)),
-                              ),
-                              Positioned(
-                                top: 10.0,
-                                left: 10.0,
-                                child: Row(
-                                  children: <Widget>[
-                                    Container(
-                                      height: 50.0,
-                                      width: 110.0,
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(20.0),
-                                          color: Colors.black.withOpacity(0.2)),
-                                      child: Center(
-                                          child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          // Icon(Icons.star, color: Colors.white, size: 12.0),
-                                          // SizedBox(width: 5.0),
+        body: ListView(
+            scrollDirection: Axis.vertical,
+            physics: AlwaysScrollableScrollPhysics(),
+            children: <Widget>[
+          userProfile(),
+          userPlace(),
+        ]));
+  }
 
-                                          Text(
-                                            '자세히보기',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 15.0,
-                                            ),
-                                          )
-                                        ],
-                                      )),
-                                    ),
-                                    //   SizedBox(width: 50.0),
-                                    //   Text(
-                                    //    'More',
-                                    //    style: TextStyle(
-                                    //        color: Colors.white,
-                                    //fontFamily: 'Opensans'
-                                    //        ),
-                                    //  ),
-                                    // SizedBox(width: 7.0),
-                                    //this should be an iconbutton in a real app.
-                                    //   Icon(Icons.arrow_drop_down, color: Colors.white, size: 25.0)
-                                  ],
-                                ),
-                              ),
-                              Positioned(
-                                top: 55.0,
-                                left: 20.0,
-                                child: Container(
-                                  width: 220.0,
-                                  child: Text(singlePlace.title,
+  Widget userProfile() {
+    return
+        //Scaffold(
+        //    body: ListView(
+        //   children: <Widget>[
+        Stack(
+      children: <Widget>[
+        Container(height: 300.0),
+        Container(
+          height: 200.0,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/1.jpg'), fit: BoxFit.cover)),
+        ),
+        Positioned(
+          top: 150.0,
+          left: 25.0,
+          child: Container(
+            height: 140.0,
+            width: 140.0,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 2.0),
+                borderRadius: BorderRadius.circular(70.0),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/2.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+        ),
+        Positioned(
+          top: 225.0,
+          left: 175.0,
+          child: Row(children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  '닉네임',
+                  style: TextStyle(
+                      fontFamily: 'SDSamliphopangcheOutline',
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '',
+                  style: TextStyle(
+                      fontFamily: 'SDSamliphopangcheOutline',
+                      fontSize: 12.0,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(width: 15.0),
+            Container(
+              height: 30.0,
+              width: 75.0,
+              decoration: BoxDecoration(
+                  color: Color(0xFFF3B287),
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Center(
+                child: Text('로그아웃',
+                    style: TextStyle(
+                        fontFamily: 'IBMPlexSansKR-Regular',
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+              ),
+            ),
+          ]),
+        ),
+      ],
+    );
+//      ],
+//    )
+    //   );
+  }
+
+  Widget userPlace() {
+    return Obx(() {
+      if (usercontroller.isLoading.value == false) {
+        return Center(
+            child: CircularProgressIndicator(
+          backgroundColor: Colors.yellow,
+        ));
+      } else {
+        return ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: usercontroller.places.length,
+            shrinkWrap: true,
+            itemBuilder: (BuildContext context, index) {
+              var singlePlace = usercontroller.places[index];
+              return Padding(
+                  padding: EdgeInsets.all(17.0),
+                  child: InkWell(
+                      onTap: () {},
+                      child: Stack(children: <Widget>[
+                        Container(
+                          height: 270.0,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.0),
+                              image: DecorationImage(
+                                  image: NetworkImage(singlePlace.image),
+                                  //              //  '${recipepost['img_url']}'),
+                                  fit: BoxFit.cover)),
+                        ),
+                        Container(
+                          height: 270.0,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: Colors.black.withOpacity(0.3)),
+                        ),
+                        Positioned(
+                          top: 10.0,
+                          left: 10.0,
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                height: 50.0,
+                                width: 110.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    color: Colors.black.withOpacity(0.2)),
+                                child: Center(
+                                    child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    // Icon(Icons.star, color: Colors.white, size: 12.0),
+                                    // SizedBox(width: 5.0),
+
+                                    Text(
+                                      '자세히보기',
                                       style: TextStyle(
-                                        //  fontFamily: 'Opensans',
-                                        fontSize: 30.0,
                                         color: Colors.white,
-                                      )),
-                                ),
+                                        fontSize: 15.0,
+                                      ),
+                                    )
+                                  ],
+                                )),
                               ),
-                              Positioned(
-                                  top: 200.0,
-                                  left: 20.0,
-                                  child: Row(children: [
-                                    Text('',
-                                        style: TextStyle(
-                                            //       fontFamily: 'Opensans',
-                                            fontSize: 30.0,
-                                            color: Colors.white)
-                                        // fontWeight: FontWeight.w600)
-                                        ),
-                                    // SizedBox(width: 15.0),
-                                    Stack(children: <Widget>[
-                                      Container(height: 40.0, width: 100.0),
-                                      //Container(
-                                      //  height: 60.0,
-                                      //  width: 60.0,
-                                      //  decoration: BoxDecoration(
-                                      //      borderRadius: BorderRadius.circular(100.0),
-                                      //  CircleAvatar(
-                                      //    backgoundImage:  NetworkImage("https://i.pinimg.com/564x/e3/54/e9/e354e97bf5d17326c3c361d884707e2c.jpg"),
-                                      //
-                                      Positioned(
-                                        left: 10.0,
-                                        child: Container(
-                                          height: 40.0,
-                                          width: 40.0,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(20.0),
-                                          ),
-                                          //      image: DecorationImage(
-                                          //    image: NetworkImage(document['chef_img']), fit: BoxFit.cover)),
-                                          child: Center(
-                                            child: Text('',
-                                                style: TextStyle(
-                                                    fontSize: 14.0,
-                                                    color: Colors.black)),
-                                          ),
-                                        ),
-                                      )
-                                    ]),
-                                  ]))
-                            ])));
-                  }));
-        }
-      })
-    ]));
+                              //   SizedBox(width: 50.0),
+                              //   Text(
+                              //    'More',
+                              //    style: TextStyle(
+                              //        color: Colors.white,
+                              //fontFamily: 'Opensans'
+                              //        ),
+                              //  ),
+                              // SizedBox(width: 7.0),
+                              //this should be an iconbutton in a real app.
+                              //   Icon(Icons.arrow_drop_down, color: Colors.white, size: 25.0)
+                            ],
+                          ),
+                        ),
+                        Positioned(
+                          top: 55.0,
+                          left: 20.0,
+                          child: Container(
+                            width: 220.0,
+                            child: Text(singlePlace.title,
+                                style: TextStyle(
+                                  //  fontFamily: 'Opensans',
+                                  fontSize: 30.0,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ),
+                        Positioned(
+                            top: 200.0,
+                            left: 20.0,
+                            child: Row(children: [
+                              Text('',
+                                  style: TextStyle(
+                                      //       fontFamily: 'Opensans',
+                                      fontSize: 30.0,
+                                      color: Colors.white)
+                                  // fontWeight: FontWeight.w600)
+                                  ),
+                              // SizedBox(width: 15.0),
+                              Stack(children: <Widget>[
+                                Container(height: 40.0, width: 100.0),
+                                //Container(
+                                //  height: 60.0,
+                                //  width: 60.0,
+                                //  decoration: BoxDecoration(
+                                //      borderRadius: BorderRadius.circular(100.0),
+                                //  CircleAvatar(
+                                //    backgoundImage:  NetworkImage("https://i.pinimg.com/564x/e3/54/e9/e354e97bf5d17326c3c361d884707e2c.jpg"),
+                                //
+                                Positioned(
+                                  left: 10.0,
+                                  child: Container(
+                                    height: 40.0,
+                                    width: 40.0,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    //      image: DecorationImage(
+                                    //    image: NetworkImage(document['chef_img']), fit: BoxFit.cover)),
+                                    child: Center(
+                                      child: Text('',
+                                          style: TextStyle(
+                                              fontSize: 14.0,
+                                              color: Colors.black)),
+                                    ),
+                                  ),
+                                )
+                              ]),
+                            ]))
+                      ])));
+            });
+      }
+    });
   }
 }
 
